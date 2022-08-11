04214e5d6e6b0a0771d5d332998c589c61b7a8ae
Izhar Ul Haq <iizharulhaq9@gmail.com>
2022-08-11 18:20:45 +0500

    added prompt to hungry

--- hungry.py
+++ hungry.py
@@ -1,8 +1,13 @@
 # hi this is just a simple code for testing git
 # for learning purpose
-a = 30
-b = 50
-if a > b:
-    print("I am hungry")
+# a = 30
+# b = 50
+# if a > b:
+#     print("I am hungry")
+# else:
+#     print("You are hungry")
+hungry = input("Are you hungry?")
+if hungry == 'yes':
+    print("Eat meal")
 else:
-    print("You are hungry")
+    print("Do your home work")
